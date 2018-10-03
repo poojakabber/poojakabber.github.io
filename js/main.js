ffcbe18fdf55b1536eeeaab4a364376d668165d4
poojakabber <pkabber@gmail.com>
2018-10-03 22:11:46 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -137,6 +137,7 @@ $(document).ready(function () {
 				div_elem.setAttribute("id", "question_div");
 				div_elem.style.width = "40vw";
 				div_elem.style.height = "80vh";
+				div_elem.style.top = "-80vh";
 				div_elem.style.fontSize = "15px";
 				div_elem.style.float = "right";
 				div_elem.style.position = "relative";
